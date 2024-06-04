03dabb3a9194d2d47b5bc75e67a1fc7ebd11a2b1
AzimJaan Akhtamov <azimaxtamov2oo2@mail.ru>
2024-06-04 14:23:42 +0500

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:alan_voice/alan_voice.dart';
+import 'package:simpleflutterapp/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,61 +12,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Alan Basic Example'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  _MyHomePageState() {
-    _initAlanButton();
-  }
-
-  void _initAlanButton() {
-    AlanVoice.addButton(
-        "314203787ccd9370974f1bf6b6929c1b2e956eca572e1d8b807a3e2338fdd0dc/prod");
-
-    AlanVoice.onCommand.add((command) {
-      debugPrint("got new command ${command.toString()}");
-      var commandName = command.data["command"] ?? "";
-      if (commandName == "showAlert") {
-        /// handle command "showAlert"
-      }
-    });
-
-    AlanVoice.onEvent.add((event) {
-      debugPrint("got new event ${event.data.toString()}");
-    });
-
-    AlanVoice.onButtonState.add((state) {
-      debugPrint("got new button state ${state.name}");
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, Alan',
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
